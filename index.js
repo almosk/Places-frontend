548f06a77fa1b0dc870ee2933204dc4a9b743c19
Alexander Moskovskiy <aleksandrmoskovskiy@a-mosk-2.local>
2020-02-11 00:20:09 +0300

    react-native-gesture-handler

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import App from './containers/App';
 import { name as appName } from './app.json';
 import { Provider } from 'react-redux';
@@ -8,9 +10,11 @@ import configureStore from './store';
 const store = configureStore()
 
 const RNRedux = () => (
-  <Provider store = { store }>
-    <App />
-  </Provider>
+  <NavigationContainer>
+    <Provider store = { store }>
+      <App />
+    </Provider>
+  </NavigationContainer>
 )
 
 AppRegistry.registerComponent(appName, () => RNRedux);
